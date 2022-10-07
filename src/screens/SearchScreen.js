2f2e0098f5073a5b0a8a9644539efc2129d9b91d
Md. Belal Hossain <belal.cseai@gmail.com>
2022-10-07 11:25:42 +0600

    loading screen and image added

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
+import { Button, View, Text, StyleSheet, Image } from "react-native";
 
 const SearchScreen = ({navigation, route}) => {
     const movie = route.params.movie;
     const [movieDetails, setMovieDetails] = useState(null);
+    const [defaultMessage, setDefaultMessage] = useState('Loading...');
 
     useEffect(() => {
         const xhr = new XMLHttpRequest();
@@ -14,21 +15,32 @@ const SearchScreen = ({navigation, route}) => {
                 let response = JSON.parse(xhr.response);
                 console.log(response);
                 setMovieDetails(response);
+
+                // If movie not found 
+                if(response?.Error) setDefaultMessage('No movie found!');
             }else{
                 console.log(`HTTP Request Failed ${xhr.status}`);
+                setDefaultMessage('No movie found!');
             }
         };
         xhr.onerror = () => {
             console.log("Error");
+            setDefaultMessage('No movie found!');
         };
     }, []);
     return ( 
         movieDetails?.Title ? <View style={styles.mainView}>
             <Text style={styles.movieTitleView}>{movieDetails?.Title}</Text>
             <Text style={styles.movieReleaseView}>{movieDetails?.Released}</Text>
+            {movieDetails?.Poster && <Image
+                style={styles.posterImage}
+                source={{
+                uri: movieDetails?.Poster,
+                }}
+            />}
             <Text style={styles.moviePlotView}>{movieDetails?.Plot}</Text>
         </View> : 
-        <View><Text style={styles.movieTitleView}>No movie found!</Text></View>
+        <View style={styles.mainView}><Text style={styles.movieTitleView}>{defaultMessage}</Text></View>
 
     );
 }
@@ -42,6 +54,7 @@ const styles = StyleSheet.create({
     movieTitleView: {
         fontSize: 20,
         textAlign: 'center',
+        color: 'orange',
     },
     movieReleaseView: {
         fontSize: 16,
@@ -52,6 +65,11 @@ const styles = StyleSheet.create({
         padding: 5,
         fontSize: 15,
     },
+    posterImage: {
+        width: 200,
+        height: 250,
+        resizeMode: 'stretch'
+    }
 });
 
 export default SearchScreen;
